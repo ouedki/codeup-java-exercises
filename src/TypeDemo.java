dd10b147a5016f7ea2ea0edf389d34c0a72b1ae5
ouedki <yassineouedki_5@hotmail.com>
2017-01-03 15:53:14 -0600

    Exercise on exploring Types in Java

---
+++ src/TypeDemo.java
@@ -0,0 +1,38 @@
+/**
+ * exercise - Exploring Types in Java
+ */
+public class TypeDemo {
+
+    //create variables of every primitive type and print them to the console
+    //try to assign a character to a variable that has been declared as an int
+    //try to define a variable with the name class
+    //initialize a variable, but do not assign it a value, then try and print it to the console
+
+    public static void main(String[] args) {
+        int number = 2000;
+        byte myByte = 127;
+        short myShort = 500;
+        long myLong = 7000;
+        float myFloat = 1.4449090987F;
+        double myDouble = 123.9999;
+        char myChar = 'A';
+        boolean myBoolean = true;
+        String myString = "hello";
+
+        System.out.println(number);
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myString);
+
+//        String class = "hello";
+//        String str;
+//        System.out.println(TypeDemo);
+//        System.out.println(str);
+
+    }
+}
