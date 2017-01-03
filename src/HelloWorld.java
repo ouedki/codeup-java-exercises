44478d7e4a0b67d9339c1f2c797ddae662846d65
ouedki <yassineouedki_5@hotmail.com>
2017-01-03 12:15:49 -0600

    First commit of HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * First java code
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Howdy, World!");
+        System.out.println("All Java code lives inside of classes");
+        System.out.println("Our program runs the 'main' method when executed");
+        System.out.println("Goodbye, world!");
+    }
+}
