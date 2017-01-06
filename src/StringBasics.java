d3fe87e40a4fc15e6c444f7c4e1e0be79503a7d6
ouedki <yassineouedki_5@hotmail.com>
2017-01-06 16:44:47 -0600

    String basics exercise

---
+++ src/StringBasics.java
@@ -0,0 +1,12 @@
+/**
+ * Exercise - String Basics
+ */
+public class StringBasics {
+    public static void main(String[] args) {
+        String message = "Check \"this\" out!, \"s inside of \"s!\n";
+        message += "In windows, the main drive is usually C:\\ \n";
+        message += "We don't need no education\n" + "We don't need no thought control \n";
+        message += "I can do back slashes \\, and double back slashes \\\\, and the amazing triple back-slash \\\\\\!\n";
+        System.out.println(message);
+    }
+}
